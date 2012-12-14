a4dddd5ce9ed1848c492030fd837c2dc1e5248ea
root <root@uDM3.(none)>
2012-12-14 16:29:17 +0100

    added Jii directory

---
+++ Jii/Jii.php
@@ -0,0 +1,292 @@
+<?php
+class Jii extends CComponent
+{
+	public  $config;
+	
+	private $_jsonizer;
+	
+	private $_bindings	= array();
+
+	private $_models 	= array();
+	
+	private $_params 	= array();
+	
+	private $_urls 		= array();
+
+	private $_functions = array();
+	
+	private $_script 	= 'jii-min-0.0.4.js';
+
+	public function init()
+	{
+		if (!isset($this->config['script'])) {
+			$this->config['script'] = $this->_script;
+		}
+
+		// publish jii script
+		$jii_js = Yii::app()->assetManager->publish(Yii::getPathOfAlias('application.components.Jii.js') . DIRECTORY_SEPARATOR .  $this->config['script']);
+		
+		// registers jii
+		Yii::app()->clientScript->registerScriptFile($jii_js, CClientScript::POS_END);
+		
+		$this->_jsonizer = new Jsonizer();
+	}
+	
+	public function jsonize($models)
+	{
+		return $this->_jsonizer->jsonize($models);
+	}
+
+	/**
+	* Adds a model to Jii
+	* @param string $name the name of the jii object property
+	* @param mixed $data the model to be added
+	*/
+	public function addModel($name, $data)
+	{
+		// if we cannot decode JSON $data, we try to jsonize
+		if (json_decode($data) === null) {			 
+			$data = $this->jsonize($data);
+		}
+		 
+		$this->_models[$name] = $data;
+	}
+
+	public function addFunction($name, $code)
+	{
+		$this->_functions[$name] = $code;
+	}
+
+	/**
+	* Allows to add custom function to be executed after document is ready
+	* @params string $function javascript anonymous function
+	*/
+	public function addBindings($function)
+	{		
+		$this->_bindings[] = $function;
+	}
+	
+	/**
+	* Converts a Php variable into a Javscript one
+	*/
+	public function addParam($name, $value)
+	{		 
+		if (!is_array($value)) {
+
+			$this->_params[$name] = $this->_toJsPrimitive($value);
+
+		} else {
+			
+			if (is_object($value) || $this->_isAssoc($value)) {
+	
+				$this->_params[$name] = json_encode($value);
+	
+			} else {
+
+				$array = '[{items}]';
+
+				$items_string = '';
+
+				foreach ($value as $item) {
+					$items_string .= $this->_toJsPrimitive($item) . ',';
+				}
+
+				$items_string = substr($items_string, 0, -1);
+
+				$array = str_replace('{items}', $items_string, $array);
+
+				$this->_params[$name] = $array;
+
+
+			}
+		}
+		
+
+	}
+	
+	private function _toJsPrimitive($value)
+	{
+		return CJavaScript::encode($value);
+	}
+
+	private function _isAssoc($arr)
+	{
+	    return array_keys($arr) !== range(0, count($arr) - 1);
+	}
+	public function addUrl($label, $url)
+	{
+		$this->_urls[$label] = '"' . htmlspecialchars($url) . '"';
+	}
+	
+	/**
+	 * The following to ensure that each script is rendered wherever you add it to your view
+	 * @return string $javascript javascript code
+	 */
+	public function getScript()
+	{
+		$models = $params = $urls = $functions = $bindings = '';
+		
+		if (!empty($this->_params)) {
+			foreach($this->_params as $name => $data) {				
+				$params .= 'jii.params.' . $name .' = ' . $data .  ';';
+			}
+					
+		}
+		
+		if (!empty($this->_urls)) {
+			foreach($this->_urls as $name => $data) {				
+				$urls .= 'jii.urls.' . $name .' = ' . $data .  ';';
+			}
+					
+		}
+		
+		if (!empty($this->_functions)) {
+			foreach($this->_functions as $name => $data) {				
+				$functions .= 'jii.functions.' . $name .' = ' . $data .  ';';
+			}
+					
+		}
+		
+		if (!empty($this->_models)) {
+			foreach($this->_models as $name => $data) {				
+				$models .= 'jii.models.' . $name .' = new jii.Model(' . $data . ');';
+			}
+		}
+
+		if (!empty($this->_bindings)) {
+			foreach ($this->_bindings as $binding) {
+				$bindings .= 'jii.bindings.bindings.push(' . $binding .');' . PHP_EOL;
+			}
+		}
+		 
+		return $urls . PHP_EOL . $params . PHP_EOL . $functions . PHP_EOL . $models . PHP_EOL . $bindings;	
+	}
+}
+
+class Jsonizer
+{	
+	
+	/**
+	* Converts a CActiveRecord instance into an array
+	* @param CActiveRecord $model
+	* @return array $model
+	*/
+	private function _jsonizeOne($model)
+	{
+		
+		// for each model we store only jsonizeables attributes
+		$attributes 	= array();
+		$jsonizeables 	= array();
+
+		// we select which attributes must be jsonized
+		if (method_exists($model, 'getJsonizeables')) {
+			$attributes = $model->getJsonizeables();
+		
+		// we get all model attributes if no jsonizeables attributes have been found
+		} else {
+			$attributes = array_keys($model->getAttributes());
+		}
+
+		// we encode each attribute into a javascript variable
+		foreach ($attributes as $attribute_name) {
+			$jsonizeables[$attribute_name] = $model->$attribute_name;
+		}
+		 
+		// basic inheritance detection
+		if ($this->_isParent('CModel', $model)) {
+			$modelArray = $jsonizeables;			
+			if (method_exists($model, 'relations')) {
+				$relations = array_keys($model->relations());
+
+				foreach ($relations as $relation) {
+					if ($model->hasRelated($relation)) {
+
+						$related_models = $model->getRelated($relation);
+						
+						if ($related_models !== null) {
+							
+							if (is_array($related_models)) {
+								
+									if (!empty($related_models)) {
+										foreach($related_models as $related) {
+	 
+											$modelArray[$relation][] = $this->_jsonizeOne($related);
+											// print_r($this->_jsonizeOne($related));
+									 	}
+									} else {
+
+										$modelArray[$relation][] = array();
+										
+									}
+
+							} else {
+								 
+								// print_r($related_models->getAttributes());
+								$modelArray[$relation] = $this->_jsonizeOne($related_models);
+							}
+
+						} else {
+							
+							$modelArray[$relation] = null;
+
+						}
+						 
+					}
+				}
+			}
+		}
+	
+		// print_r($modelArray);
+		return $modelArray;			
+	}	
+	
+	/**
+	* Converts an array of CActiveRecord instances into a php array
+	* @param array $models
+	* @return array 
+	*/
+	private function _jsonize($models)
+	{	
+		$modelArray = array();
+		$i = 0;
+		 
+		foreach ($models as $model) {
+			$model->getAttributes();
+			$modelArray[$i++] = $this->_jsonizeOne($model);
+		}
+		
+		return $modelArray;
+	}
+	
+	private function _isParent($classname, $child)
+	{
+		while(($parent = get_parent_class($child)) !== false) {
+			if ($parent === $classname) {
+				return true;
+			}
+			$child = $parent;
+		}
+		return false;
+	}
+	
+	/**
+	* Converts CModel instances into JSON objects
+	* @param CModel $data
+	* @return string $json
+	*/
+	public function jsonize($data)
+	{
+		// support for CActiveDataProvider
+		if ($data instanceof CActiveDataProvider) {
+			$data = $data->getData();
+		}
+		
+		if (is_array($data)) {
+			return json_encode($this->_jsonize($data));
+		} else {
+			return json_encode($this->_jsonizeOne($data));
+		}
+		
+	}	
+		
+}
---
+++ Jii/js/jii-0.0.4.js
@@ -0,0 +1,203 @@
+(function (window, ko){
+	
+		// jii object
+	var jii 			= {},
+	
+		// Model constructor
+		Model       	= {};
+		 
+	// object container for utilities
+	jii.utils 		= {};
+
+	// object container for params
+	jii.params 		= {};
+	
+	// object container for Models
+	jii.models 		= {};
+	
+	// object container for urls
+	jii.urls		= {};
+	
+	// object container for functions
+	jii.functions 	= {};	
+
+	// object used to contain functions that need to be executed when document is ready
+	jii.bindings    = {
+		bindings: [],
+		apply: function () {
+			var s = jii.bindings.bindings.length,
+				i = 0;
+			for (i=0; i < s; i++) {
+				jii.bindings.bindings[i]();
+			}
+		}
+	};
+	/*
+	* Copyright 2010, John Resig
+	* Dual licensed under the MIT or GPL Version 2 licenses.
+	* http://jquery.org/license
+	*/
+	// Cleanup functions for the document ready method
+	// attached in the bindReady handler
+	if ( document.addEventListener ) {
+	DOMContentLoaded = function() {
+	    document.removeEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+	};
+
+	} else if ( document.attachEvent ) {
+	DOMContentLoaded = function() {
+	    // Make sure body exists, at least, in case IE gets a little overzealous 
+	            if ( document.readyState === "complete" ) {
+	        document.detachEvent( "onreadystatechange", DOMContentLoaded ); 
+	    }
+	    };
+	}
+
+	// Catch cases where $(document).ready() is called after the
+	// browser event has already occurred.
+	if ( document.readyState === "complete" ) {	    
+	    // Handle it asynchronously to allow scripts the opportunity to delay ready
+		//return setTimeout( jQuery.ready, 1 );
+	    // ^^ you may want to call *your* function here, similarly for the other calls to jQuery.ready
+	    setTimeout( jii.bindings.apply, 1 );
+	}
+
+	// Mozilla, Opera and webkit nightlies currently support this event
+	if ( document.addEventListener ) {
+	    // Use the handy event callback
+		document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+		// A fallback to window.onload, that will always work
+		//window.addEventListener( "load", jQuery.ready, false );
+	    window.addEventListener( "load", jii.bindings.apply, false );
+	 // If IE event model is used
+	 } else if ( document.attachEvent ) {
+	        // ensure firing before onload,
+	        // maybe late but safe also for iframes
+	        document.attachEvent("onreadystatechange", DOMContentLoaded);
+
+	        // A fallback to window.onload, that will always work
+	        window.attachEvent( "onload", jii.bindings.apply );
+
+	 }
+
+	// Jii Model constructor
+	Model = function(){	 
+		
+		var array = arguments[0] instanceof Array  ? true : false;
+		
+		this.count = function(){
+			var counter = 0;
+		 	if (array) {
+				for (var attr in this) {
+					if (isNaN(parseInt(attr, 10)) === false) {
+						counter++;
+					}
+				}
+			} else {
+				counter = 1;
+			}
+			return counter;
+		};
+
+		/**
+		* Returns the Javascript representation of the Model object
+		* @param Model The model to decode
+		* @return object js_object the decoded model
+		*/
+		this.toJS = function() {
+			var js_object = {};
+
+			for (var attr in this) {
+				if (typeof this[attr] !== "function") {
+					js_object[attr] = this[attr];
+				}
+			}
+			return js_object;
+		}
+
+		// performs some initialization taks
+		if (array) {
+			this.add = function(){
+				this[this.count()] = arguments[0];
+			};
+		}
+		
+		for (var attr in arguments[0]) {
+			this[attr] = arguments[0][attr];
+		}
+	}
+	
+	/**
+	* function findByAttribute
+	* @param object {attribute: "attr", value: val}
+	* @return Model
+	*/
+	Model.prototype.findByAttribute = function(){
+		if (typeof arguments[0] === "undefined") {
+			throw Error("You must provide both attribute and value");
+		}		 
+		var source 	  = this,
+			cnt 	  = this.count(),
+			i 		  = 0,
+			attribute = arguments[0].attribute,
+			value	  = arguments[0].value;
+
+			if (cnt > 1) {
+				for (i=0; i < cnt; i++) {					 
+					if (source[i][attribute] === value) {
+						return source[i];
+						break;
+					}
+				}
+			} else {					
+				if (typeof source[0] !== "undefined") {
+					for (var attr in source[0]) {
+						if (source[0][attribute] === value) {
+							return source[0];
+							break;
+						}	
+					}
+				} else if (source[attribute] === value) {
+					return source; 
+				}
+			}
+			
+			
+			return null;
+	}
+		
+	
+	// follow some utilities
+	jii.utils.observable = function(){
+		if (typeof ko ==="undefined") {
+			throw Error("ko has not been found");	
+		}
+		return ko.observable(arguments[0]);
+	}
+		
+	jii.utils.observableArray = function(){
+		if (typeof ko ==="undefined") {
+			throw Error("ko has not been found");	
+		}
+		return ko.observableArray(arguments[0].toJS());
+	}
+
+	jii.utils.getObservable = function(){
+		if (typeof ko ==="undefined") {
+			throw Error("ko has not been found");	
+		}
+
+		if (isArray(arguments[0])){
+			return ko.observableArray(arguments[0]);
+		} else {
+			return ko.observable(arguments[0]);
+		}
+	}
+
+	// add Model constructor to jii
+	jii.Model = Model;
+
+	// makes jii available on the global scope
+	window.jii = jii;
+
+}(window, ko))
